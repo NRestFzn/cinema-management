5cc30997f5fa14564504c273efe084f39edf13e7
Nashir Resta <nashirresta7@gmail.com>
2025-07-14 10:56:35 +0700

    fix: class property

--- Dto/ApiResponseDto.cs
+++ Dto/ApiResponseDto.cs
@@ -6,20 +6,25 @@ using System.Text.Json.Serialization;
 
 namespace CinemaManagement.Dto
 {
-    public class ApiResponseDto<T>
+    public class ApiResponseDto
     {
-
         public bool Success { get; set; }
-        public T? Data { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Message { get; set; }
+    }
+    public class GetDataResponseDto<T> : ApiResponseDto
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public T? Data { get; set; }
+    }
 
+    public class ErrorResponseDto : ApiResponseDto
+    {
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public int? ErrorCode { get; set; } = null;
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public List<string>? Errors { get; set; } = null;
     }
-
 }
